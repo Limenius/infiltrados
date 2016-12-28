aec100690ac131bfc92b97390183a231c6e843a2
Nacho Martín <nacho@limenius.com>
2016-12-29 00:20:44 +0100

    More identifier

--- assets/js/identify.js
+++ assets/js/identify.js
@@ -4,16 +4,23 @@ class identify {
     constructor () {
         this.userIds = [];
         this.setupEvents();
+        this.inModal = null;
     }
 
     setupEvents () {
         $().ready(() => {
             for (let id of this.userIds) {
-                $('article[userId='+id+'] .read-more-btn').click(() => {
+                $('article[userId='+id+'] .read-more-btn').click((evt) => {
+                    this.inModal = $(evt.target).parents('article').attr('userId');
                     $('#identifyModal').modal()
                 });
             }
             $("select").select2();
+            $("#doIdentify").click(() => {
+                const selected = $('#identiselect').val();
+                const inModal = this.inModal;
+                console.log(`en modal de ${inModal}. Seleccionado ${selected}.`);
+            });
         });
     }
 
